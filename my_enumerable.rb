5668aeabc3fc8a79bf2f567901a07c0a00edf270
Solomon Achilles <solohag92@gmail.com>
2022-06-09 14:39:23 +0000

    Add methods to enumerable module

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,2 +1,25 @@
 module MyEnumerable
+  def all?
+    response = false
+    @list.each do |num|
+      response = true if yield(num)
+    end
+    response
+  end
+
+  def any?
+    response = false
+    @list.each do |num|
+      response = true if yield(num)
+    end
+    response
+  end
+
+  def filter?
+    filtered_list = []
+    @list.each do |num|
+      filtered_list.push(num) if yield(num)
+    end
+    filtered_list
+  end
 end
--- my_list.rb
+++ my_list.rb
@@ -1,14 +1,22 @@
-require 'my_enumerable'
+require_relative 'my_enumerable'
 
 class MyList
-    include MyEnumerable
+  include MyEnumerable
 
-    def initialize(list)
-        @list = list
-    end
+  def initialize(*list)
+    @list = list
+  end
 
-    def each
-        (0...@list.length).each do |i| 
-            yield @list[i]
+  def each
+    (0...@list.length).each do |i|
+      yield @list[i]
     end
+  end
 end
+
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 6 })
+puts(list.filter?(&:even?))
